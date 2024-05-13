6c9a030055448088dae633da1c1ca1ba352836b7
Weverton Lima <wevertonslima@gmail.com>
2024-05-13 15:44:09 -0300

    building fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,9 +10,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
