7fe560d2a9bb1c08c7af38489bf6cc89e835d482
Weverton Lima <wevertonslima@gmail.com>
2024-05-13 15:19:50 -0300

    fix element

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -11,14 +11,14 @@ defineProps({
     <Head title="Privacy Policy" />
 
     <div :class="{'dark': $page.props.userPreferences.darkMode}" class="font-sans antialiased">
-        <div class="pt-4 bg-gray-100 dark:bg-gray-800">
+        <div class="pt-4 bg-gray-100 dark:bg-gray-900">
             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                 <div>
                     <AuthenticationCardLogo />
                 </div>
 
-                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-900 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-dark" v-html="policy">
-                    <p class="text-gray-900 dark:text-gray-100">Please review our Privacy Policy to understand our practices.</p>
+                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-900 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-dark">
+                    <div v-html="policy" class="text-gray-900 dark:text-gray-100 selection:bg-blue-300 dark:selection:bg-blue-800 selection:text-white"></div>
                 </div>
             </div>
         </div>
