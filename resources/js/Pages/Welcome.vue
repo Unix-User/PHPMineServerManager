235891837f596e253bf68758d510ef5e950ad04e
UnixUser <38821945+Unix-User@users.noreply.github.com>
2025-03-25 20:57:54 -0300

    fix prices

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -418,7 +418,7 @@ const scrollToSection = (sectionId, offset = 100) => {
                             <Link :href="route('shop')"
                                 class="btn-primary btn-lg w-full flex items-center justify-center hover:bg-opacity-90 transition-colors duration-200 text-lg font-semibold rounded-md">
                                 <i class="fa-solid fa-shopping-cart mr-2"></i>
-                                <span>Comprar por R$ {{ parseFloat(item.price).toFixed(2) }}</span>
+                                <span>Comprar por R$ {{ (item.price / 100).toFixed(2) }}</span>
                             </Link>
 
                             <!-- Selo de Garantia e Benefícios -->
